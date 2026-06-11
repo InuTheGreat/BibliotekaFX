364e43e35cafebc2c560b22227ea86ec04d91365
InuTheGreat <konrad.mrozowski@tutamail.com>
2026-06-11 12:03:52 +0200

    test Zegara

--- src/main/java/pl/controller/pane/bottom/PaneBottomPaneController.java
+++ src/main/java/pl/controller/pane/bottom/PaneBottomPaneController.java
@@ -1,15 +1,17 @@
 package pl.controller.pane.bottom;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 import pl.library.model.User;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class PaneBottomPaneController {
-
 
     @FXML
     private Label dateLabel;
@@ -22,7 +24,17 @@ public class PaneBottomPaneController {
 
     public void setupFooter(User user) {
         loginLabel.setText("Zalogowany jako: " + user.getRole());
-        dateLabel.setText("Data zalogowania: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
         dbLabel.setText("Baza: MySQL");
+        startClock();
+    }
+
+    private void startClock() {
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            dateLabel.setText(LocalDateTime.now()
+                    .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        }), new KeyFrame(Duration.seconds(1)));
+
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
     }
 }
